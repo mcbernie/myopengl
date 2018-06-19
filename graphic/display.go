9ca76a169121309953ad7dcd30a337ff721e3f87
Nicolas Brüggemann <nbrueggemann@itcoops.de>
2018-06-19 13:13:39 +0200

    remove comments and add video testing

--- graphic/display.go
+++ graphic/display.go
@@ -1,10 +1,6 @@
 package graphic
 
 import (
-	"log"
-
-	//"github.com/go-gl/gl/v4.1-core/gl" // OR:
-	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/mcbernie/myopengl/gfx"
 	"github.com/mcbernie/myopengl/slideshow"
 )
@@ -15,7 +11,6 @@ type Display struct {
 	windowHeight float32
 
 	defaultShader *gfx.Program
-	gui           *GUI
 	slideshow     *slideshow.Slideshow
 }
 
@@ -42,67 +37,9 @@ func (d *Display) SetWindowSize(width, height int) {
 	d.slideshow.UpdateWindowSize(float32(width), float32(height))
 }
 
-func (d *Display) setupVbp() {
-	//var VBO uint32
-	/*gl.GenBuffers(1, &VBO)
-	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
-	gl.BufferData(gl.ARRAY_BUFFER, len(d.box)*4, gl.Ptr(d.box), gl.STATIC_DRAW)
-
-	gl.EnableVertexAttribArray(0)
-	gl.VertexAttribPointer(0, 2, gl.FLOAT, false, 0, gl.PtrOffset(0))*/
-}
-
 //Render make all updates for rendering
 func (d *Display) Render(time float64) {
-
-	// wait for incoming gl commands and run...
-
 	d.slideshow.Render(time)
-}
-
-//GUI show struct
-type GUI struct {
-	width  float64
-	height float64
-
-	x float64
-	y float64
-}
-
-func (d *Display) MakeGUI(x float64, y float64, width float64, height float64) *GUI {
-
-	calcX := (x / float64(d.windowWidth))
-	calcY := (y * -1 / float64(d.windowHeight)) - height
-
-	log.Println(calcY)
-	//calcX := xPointZero
-	//calcY := yPointZero
-
-	return &GUI{
-		width:  width / float64(d.windowWidth),
-		height: height / float64(d.windowHeight),
-		x:      calcX,
-		y:      calcY,
-	}
-
-}
-
-func (g *GUI) draw() {
-	//d.defaultShader.Use()
-	gl.UseProgram(0)
-	//d.drawString(0.0, 0.0, "Hallo Mallo Welt und Geld", 12)
-
-	gl.Disable(gl.TEXTURE_2D)
-
-	gl.Disable(gl.LIGHTING)
-	gl.Color3d(1.0, 1.0, 1.0)
-	gl.Begin(gl.QUADS)
-	gl.Vertex3d(g.x, g.y+g.height, 0)
-	gl.Vertex3d(g.x+g.width, g.y+g.height, 0)
-	gl.Vertex3d(g.x+g.width, g.y, 0)
-	gl.Vertex3d(g.x, g.y, 0)
-	gl.End()
-	gl.Enable(gl.TEXTURE_2D)
 }
 
 //Delete unload all data from gpu
